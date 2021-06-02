aaa54e662c9a0780e386348b062e9b6396cbc8ae
Josh Deltener <josh.deltener@realtruck.com>
2021-06-02 15:35:35 -0500

    omg kill me

--- modules/snarf/myModule2.js
+++ modules/snarf/myModule2.js
@@ -1,10 +1,13 @@
-this.nuxt.hook('render:setupMiddleware', (app) => {
-    app.use('/test1/snarf', test1)
-})
+export default function(){
+    this.nuxt.hook('render:setupMiddleware', (app) => {
+        app.use('/test1/snarf', test1)
+    })
 
-function test1(req, res, next){    
-    console.log('myModule2')    
-    res.end(JSON.stringify({
-        snarf:'blat'
-    }))
+    function test1(req, res, next){        
+        console.log('myModule2')
+        
+        res.end(JSON.stringify({
+            snarf: 'blat'
+        }))
+    }
 }
